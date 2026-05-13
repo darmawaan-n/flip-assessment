c7e91e7f1e20413e08edc64eebf93c7f59916f99
Nur Rochman Darmawan <nur.darmawan@myrepublic.net.id>
2026-05-14 00:52:33 +0700

    add validation to user's request

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,27 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import List
 
 
 class PokemonProcessRequest(BaseModel):
     """Validates the incoming JSON request."""
 
-    raw_id: str = Field(..., min_length=13, max_length=13, example="7dsa8d7sa9dsa")
-    user_id: str = Field(..., min_length=7, max_length=7, example="5199434")
-    pokemon_ability_id: str = Field(..., example="150")
+    raw_id: str = Field(default=None, min_length=13, max_length=13, example="7dsa8d7sa9dsa")
+    user_id: str = Field(default=None, min_length=7, max_length=7, example="5199434")
+    pokemon_ability_id: str = Field(example="150")
+
+    @field_validator("user_id")
+    @classmethod
+    def check_user_id_numeric(cls, value: str):
+        if not value.isdigit():
+            raise ValueError("user_id must contain digit-only characters")
+        return value
+
+    @field_validator("raw_id")
+    @classmethod
+    def check_raw_id_alnum(cls, value: str):
+        if not value.isalnum():
+            raise ValueError("raw_id must contain alpha-numeric-only characters")
+        return value
 
 
 class LanguageSchema(BaseModel):
